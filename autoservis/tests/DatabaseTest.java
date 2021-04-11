87e982a20557a02e347485ab0c4455c6b75086ba
JartaB <jarda.blechac@gmail.com>
2021-04-11 12:40:42 +0200

    testy databáze
    test připojení a select

---
+++ autoservis/tests/DatabaseTest.java
@@ -0,0 +1,41 @@
+import org.junit.jupiter.api.Test;
+import sample.Database;
+
+
+import java.sql.*;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DatabaseTest {
+    Connection connection = null;
+    Statement statement = null;
+    ResultSet resultSet = null;
+    String url = "jdbc:mysql://localhost:3306/autoservis";
+    String user = "root";
+    String pass = "";
+    @Test
+    void DataNotEmpty() {
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(url, user, pass);
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery("select * from objednani");
+            while (resultSet.next()) {
+                assertEquals(resultSet.getString("name") != null, true);
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Test
+    void getConnectionTest() throws SQLException {
+        Connection connection = Database.getConnection();
+        assertEquals(connection != null, true);
+    }
+}
